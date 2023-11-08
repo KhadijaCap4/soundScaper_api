1e01db67c521d506faa1cbc32b47338e14b5d788
KhadijaCap4 <khadija.sy@cap4lab.com>
2023-11-08 12:15:26 +0100

    j

--- documentations/description.py
+++ documentations/description.py
@@ -3,7 +3,7 @@ Pour obtenir votre accès :
 1. Créer un compte sur /auth/signup
 2. Laissez-vous rediriger sur le lien du [checkout](/stripe/checkout)
 
-# Spécification
+# Spécification 
 Cette API puissante vous permet de gérer de manière exhaustive vos listes de lecture musicales et les utilisateurs de votre plateforme.
 Elle propose des endpoints pour créer, modifier, supprimer ou répertorier des morceaux de musique dans votre liste de lecture, ainsi que pour gérer les utilisateurs de votre plateforme.
 De plus, elle intègre des fonctionnalités d'authentification sécurisée et des options de paiement via Stripe.
