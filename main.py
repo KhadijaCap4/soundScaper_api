ed98b32b7d251cf86a6ee97d1dd5b1965408d2f3
KhadijaCap4 <khadija.sy@cap4lab.com>
2024-05-10 11:49:49 +0200

    update main

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 from fastapi import FastAPI
 from routers.router_todos import router as todos_router
-from documentations.description import api_description
-from documentations.tags import tags_metadata
+
 
 app = FastAPI(
     title="TodoList API",
-    description=api_description,
-    openapi_tags=tags_metadata,
     docs_url='/'
 )
 
